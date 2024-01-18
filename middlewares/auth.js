8238ee9fe4ed578d22679eb1f9058563c3c7a896
v02r <vmprio@yandex.ru>
2024-01-18 21:44:02 +0300

    fix dotenv

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedErr = require('../errors/UnauthorizedErr');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
 console.log(req.header("Authorization"));
-    let payload;
-try{
-const token = req.header("Authorization").split(" ")[1];
-    payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
+  const JWT_SECRET = process.env.JWT_SECRET || 'some-secret-key';
+  let payload;
+
+  try{
+    const token = req.header("Authorization").split(" ")[1];
+  payload = jwt.verify(token, JWT_SECRET);
+    } catch (err) {
     throw new UnauthorizedErr('Необходима авторизация');
   }
   req.user = payload;
